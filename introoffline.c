ce7296631354ea5e25a6b844143fab6dd3825bf7
pseudostripy <41550973+pseudostripy@users.noreply.github.com>
2021-01-30 23:10:19 +0000

    First compiled attempt with translated CE
    Note: crashes after menu screen.

--- introoffline.c
+++ introoffline.c
@@ -43,128 +43,96 @@ void setup_d8proxy(void)
 
 void attach_hook(void)
 {
-    {
-	/* no logo modification    */
-        struct patch patches[] =
-	{
-	    /* release */
-	    {
-		0x8320b0, 7,
-		{0xe9, 0x27, 0x01, 0x00, 0x00, 0x90, 0x90,},
-		{0xff, 0x24, 0x85, 0x24, 0x22, 0xc3, 0x00,},
-	    },
-	    {
-		0x8322b3, 2,
-		{0x90, 0x90,},
-		{0x74, 0x0d,},
-	    },
-	    /* debug */
-	    {
-		0x831b30, 7,
-		{0xe9, 0x27, 0x01, 0x00, 0x00, 0x90, 0x90,},
-		{0xff, 0x24, 0x85, 0xa4, 0x1c, 0xc3, 0x00,},
-	    },
-	    {
-		0x831d33, 2,
-		{0x90, 0x90,},
-		{0x74, 0x0d,},
-	    },
-	    /* steamworks */
-	    {
-		0x831180, 7,
-		{0xe9, 0x27, 0x01, 0x00, 0x00, 0x90, 0x90,},
-		{0xff, 0x24, 0x85, 0xf4, 0x12, 0xc3, 0x00,},
-	    },
-	    {
-		0x831383, 2,
-		{0x90, 0x90,},
-		{0x74, 0x0d,},
-	    },
-	};
+    void *base_addr = GetModuleHandle(NULL);
 	
-	void *base_addr = GetModuleHandle(NULL);
-	for (int i = 0; i < (sizeof(patches) / sizeof(patches[0])); i++) {
-	    struct patch *patch = patches + i;
-	    void *addr = base_addr + patch->rel_addr;
-	    DWORD size = patch->size;
-
-	    if (memcmp(addr, patch->orig, size) == 0) {
-		DWORD old;
-		VirtualProtect(addr, size, PAGE_EXECUTE_READWRITE, &old);
-		memcpy(addr, patch->patch, size);
-		VirtualProtect(addr, size, old, &old);
-	    }
-	}
-    }
-
-    {
-	/* setting game to offline */
-	static char hook_bytes[] =
+	/*
+	 Add the "Open No Baby Jump Mod" code
+	*/
+	static char spd_bytes[] =
 	{
-	    0x05, 0x4c, 0x0b, 0x00, 0x00,   /* add eax, 0xb4c          */
-	    0xc6, 0x00, 0x01,               /* mov byte ptr [eax], 0x1 */
-	    0x40,                           /* inc eax                 */
-        0xc6, 0x00, 0x00,               /* mov byte ptr [eax], 0x0 */
-	    0xe8, 0x00, 0x00, 0x00, 0x00,   /* call original           */
-	    0xe9, 0x00, 0x00, 0x00, 0x00,   /* jmp back to game code   */
+		// CE newmem:
+		0x50, 										// push eax
+		0x8B, 0xC6, 								// mov eax, esi
+		0x3C, 0x10, 								// cmp al,10
+		0x75, 0x0A, 								// jne code
+		0x0F, 0x1F, 0x40, 0x00, 					// nop dword ptr [eax+00]
+		0x89, 0x35, 0x00, 0x00, 0x00, 0x00,			// mov [speedBse_xxxx],esi
+		0x58, 										// code: pop eax
+		0x0F, 0x11, 0x96, 0xE0, 0x00, 0x00, 0x00, 	// movups [esi+000000E0],xmm2
+		0xE9, 0x00, 0x00, 0x00, 0x00, 				// jmp return_xxxx
+		// speedBse:
+		0x00, 0x00, 0x00, 0x00, 
 	};
-	void *hook = hook_bytes;
-
-	void *fps_injct_pnt;
-        char *reg_injct_pnt;
-	DWORD ver_num = *(DWORD*) 0x400080;
-	if (ver_num == 0xfc293654) {
-            /* release */
-	    fps_injct_pnt = (void*)0xebba94;
-            reg_injct_pnt = (char*)0x7068cf;
-        } else if (ver_num == 0xce9634b4) {
-            /* debug */
-	    fps_injct_pnt = (void*)0xebe952;
-            reg_injct_pnt = (char*)0x7078df;
-	} else if (ver_num == 0xe91b11e2) {
-            /* steamworks (+cracked?) */
-	    fps_injct_pnt = (void*)0xeba704;
-            reg_injct_pnt = (char*)0x7069cf;
-        } else
-	    return;
-
-	DWORD op;
-	VirtualProtect(hook_bytes, sizeof(hook_bytes),
-		       PAGE_EXECUTE_READWRITE, &op);
-	VirtualProtect(fps_injct_pnt, 5, PAGE_EXECUTE_READWRITE, &op);
-
-	int *offset_loc;
-	int  new_offset;
-	/* get address the  game is supposed to call */
-	offset_loc   = fps_injct_pnt + 1;
-	new_offset   = *offset_loc;
-	new_offset  += (int)fps_injct_pnt + 5;
-	new_offset  -= (int)hook + 17;
-	offset_loc   = hook + 13;
-	*offset_loc  = new_offset;
-
-	/* get the address our code jumps back to */
-	new_offset   = (int)fps_injct_pnt + 5;
-	new_offset  -= (int)hook + 22;
-	offset_loc   = hook + 18;
-	*offset_loc  = new_offset;
-
-	/* write the jump to our code over the game's */
-	new_offset   = (hook - fps_injct_pnt) - 5;
-	*(char*) fps_injct_pnt++ = 0xe9;
-	offset_loc   = fps_injct_pnt;
-	*offset_loc  = new_offset;
-
-	VirtualProtect(fps_injct_pnt, 5, op, &op);
-
-        /* make the game set itself to 'regular' offline
-         * mode, i.e. as if offline on steam
-         */
-        VirtualProtect(reg_injct_pnt, 1,
-                       PAGE_EXECUTE_READWRITE, &op);
-        *reg_injct_pnt = 0x00;
-        VirtualProtect(reg_injct_pnt, 1, op, &op);
-    }
+	void *spdhook = spd_bytes;
+	void *speedBse = &spd_bytes[30];
+	
+	struct patch spd_patch = 
+	    {
+			0x3AAB05, 										// "GetSpdPtrAOB"  
+			7,
+			{0xE9, 0x00, 0x00, 0x00, 0x00, 0x90, 0x90,}, 	// jmp [xxxx], nop, nop
+			{0x0F, 0x11, 0x96, 0xE0, 0x00, 0x00, 0x00,}, 	// movups [esi + 000000E0],xmm2
+	    };
+		
+	void *spdinjaddr = base_addr + spd_patch.rel_addr;
+	char *spdinj_ret = (char*) spdinjaddr + 7;
+	
+	// Calc and update addresses
+	memcpy(&spd_patch.patch[1],&spdhook,4); 		// jump to spd_bytes code
+	memcpy(&spd_bytes[13],&speedBse,4); 			// fix mov [speedBse]
+	memcpy(&spd_bytes[26],&spdinj_ret,4); 			// return to spdinject (+offset)
+	
+	// Edit memory:
+	DWORD size = spd_patch.size;
+	if (memcmp(spdinjaddr, spd_patch.orig, size) == 0) {
+		DWORD old;
+		VirtualProtect(spdinjaddr, size, PAGE_EXECUTE_READWRITE, &old);
+		memcpy(spdinjaddr, spd_patch.patch, size);
+		VirtualProtect(spdinjaddr, size, old, &old);
+	}
+	
+	/*
+	 Add the "no baby jump variant 1 (full)" code
+	*/
+	static char jump_bytes[] =
+	{
+		// CE newmem:
+		0x8B, 0xC8,										// mov ecx, eax
+		0x8B, 0x42, 0x10, 								// mov eax,[edx+10]
+		0x0F, 0x51, 0xC0, 								// sqrtps xmm0,xmm0
+		0x50, 											// push eax
+		0xA1, 0x00, 0x00, 0x00, 0x00, 					// mov eax,[speedBse_xxxx]
+		0xF3, 0x0F, 0x10, 0x80, 0xE8, 0x00, 0x00, 0x00, // movss xmm0, [eax+E8]
+		0x58, 											// pop eax
+		0xE9, 0x00, 0x00, 0x00, 0x00, 					// jmp [xxxx]
+	};
+	void *jumphook = jump_bytes;
+		
+	struct patch jump_patch = 
+	    {
+			0x3A09C4, 											// "NoBabyJAOB1"  
+			8,
+			{0xE9, 0x00, 0x00, 0x00, 0x00, 0x90, 0x90, 0x90}, 	// jmp [xxxx], nop, nop, nop
+			{0x8b, 0xC8, 0x8B, 0x42, 0x10, 0x0F, 0x51, 0xC0}, 	// movups [esi + 000000E0],xmm2
+	    };
+		
+	void *bbjinjaddr = base_addr + jump_patch.rel_addr;
+	char *nobbjinj_ret = (char*) bbjinjaddr + 8;
+	
+	// Calc and update addresses
+	memcpy(&jump_patch.patch[1],&jumphook,4); 		// jump to jumphook_bytes code
+	memcpy(&jump_bytes[10],&speedBse,4); 			// fix mov eax [speedBse]
+	memcpy(&jump_bytes[24],&nobbjinj_ret,4); 		// return to bbjinject (+offset)
+	
+	// Edit memory:
+	size = jump_patch.size;
+	if (memcmp(bbjinjaddr, jump_patch.orig, size) == 0) {
+		DWORD old;
+		VirtualProtect(bbjinjaddr, size, PAGE_EXECUTE_READWRITE, &old);
+		memcpy(bbjinjaddr, jump_patch.patch, size);
+		VirtualProtect(bbjinjaddr, size, old, &old);
+	}
+	
 }
 
 BOOL APIENTRY DllMain(HMODULE mod, DWORD reason,
